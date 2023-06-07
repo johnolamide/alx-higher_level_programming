7b7e61fbf1455930161e7ab3da9b2f466a03c52e
Olamide Alokan <alokanolamide@gmail.com>
2023-06-07 03:03:52 -0700

    [ADD] 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{:d}".format(number % 10), end="")
+    last_digit = number % 10
+    print("{:d}".format(last_digit), end="")
+    return (last_digit)
