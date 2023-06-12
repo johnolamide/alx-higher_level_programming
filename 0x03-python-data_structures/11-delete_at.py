70015497f964e50e457cd5c6efcbc3585a82318b
Olamide Alokan <alokanolamide@gmail.com>
2023-06-12 11:43:27 -0700

    [ADD] 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -12,8 +12,6 @@ def delete_at(my_list=[], idx=0):
     """
     if not my_list:
         return None
-    if (idx >= len(my_list)):
-        return my_list
     del my_list[idx]
     return my_list
 
