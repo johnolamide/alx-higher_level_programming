25208bbd15c4a520d8cdbf92f58f0baeb4f42bca
Olamide Alokan <alokanolamide@gmail.com>
2023-06-12 23:26:45 -0700

    [FIXED] 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,8 +11,8 @@ def delete_at(my_list=[], idx=0):
         modified list
     """
     if not my_list:
-        return None
-    if (idx >= len(my_list)):
+        return []
+    if (idx >= len(my_list) or idx < 0):
         return my_list
     del my_list[idx]
     return my_list
