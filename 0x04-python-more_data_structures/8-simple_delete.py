1b59ff397f6c1527a48e2f8aa545ff2e4ee43db4
Olamide Alokan <alokanolamide@gmail.com>
2023-06-14 12:12:10 -0700

    [ADD] 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
+def simple_delete(a_dictionary, key=" "):
     """deletes a key in a dictionary
 
     Args:
@@ -11,9 +11,12 @@ def simple_delete(a_dictionary, key=""):
     """
     if key in a_dictionary:
         del a_dictionary[key]
+    return a_dictionary
 
 
 if __name__ == "__main__":
+    print_sorted_dictionary = __import__(
+                    '6-print_sorted_dictionary').print_sorted_dictionary
     a_dictionary = {'language': "C", 'Number': 89,
                     'track': "Low", 'ids': [1, 2, 3]}
     new_dict = simple_delete(a_dictionary, 'track')
