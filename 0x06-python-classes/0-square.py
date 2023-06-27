fc205cd88def46951c6d7be42e95f6f630530bd4
Olamide Alokan <alokanolamide@gmail.com>
2023-06-27 07:54:57 -0700

    [FIXED] 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" Module contains: an empty Square class"""
 class Square:
     """an empty class that defines a square
     """
     pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
