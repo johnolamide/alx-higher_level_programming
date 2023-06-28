e078de4be7570277d56c539cf5bad38ffcfeb65a
Olamide Alokan <alokanolamide@gmail.com>
2023-06-28 01:20:23 -0700

    [FIXED] 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
     """
     count = 0
     if not my_list:
-        return []
+        return count
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
