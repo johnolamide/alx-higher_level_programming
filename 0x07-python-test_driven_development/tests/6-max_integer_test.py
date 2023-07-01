616eebb19007e220a8a9416df150d74e690c720f
Olamide Alokan <alokanolamide@gmail.com>
2023-07-01 18:05:30 +0100

    [ADD] tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,7 +6,8 @@
 
 
 import unittest
-max_integer =  __import__('6-max_integer').max_integer
+max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """
