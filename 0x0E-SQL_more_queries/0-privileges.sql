5d8b564a18ef48d4b7a12eb4929c7108b702f1ea
Olamide Alokan <alokanolamide@gmail.com>
2023-08-16 17:28:08 +0100

    [ADD] 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all privileges of MySQL users
-SELECT *
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2') AND Host = 'localhost'
+SHOW GRANTS FOR
+'user_0d_1'@'localhost';
+SHOW GRANTS FOR
+'user_0d_2'@'localhost';
