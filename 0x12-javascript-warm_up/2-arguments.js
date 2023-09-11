c6b9453b5036b44765125a62a34f56200d30d125
Olamide Alokan <alokanolamide@gmail.com>
2023-09-11 21:03:40 +0100

    [FIXED] 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv.length > 2) {
+if (argv.length > 3) {
+  console.log('Arguments found');
+} else if (argv.length > 2) {
   console.log('Argument found');
 } else {
   console.log('No argument');
