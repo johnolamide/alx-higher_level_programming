e5a7cedd6d29d72aae7574094078bb3acdecbf54
Olamide Alokan <alokanolamide@gmail.com>
2023-09-11 16:46:46 +0100

    [ADD] 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 const first = Number(argv[2]);
 const second = Number(argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   const result = a + b;
   return result;
 }
