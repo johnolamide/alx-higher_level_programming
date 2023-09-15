46db5c0f82546792d32e7a7eb878526c5f594c90
Olamide Alokan <alokanolamide@gmail.com>
2023-09-15 14:49:20 +0100

    [ADD] 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,7 +20,7 @@ def list_states(username, password, database_name):
             db=database_name
         )
     except MySQLdb.Error:
-       pass
+        pass
 
     cur = conn.cursor()
     query = "SELECT * FROM states ORDER BY id ASC"
