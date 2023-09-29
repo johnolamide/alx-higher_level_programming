3f3e96c4cfd3587505ed2a6910292ea4bf833a75
Olamide Alokan <alokanolamide@gmail.com>
2023-09-29 21:00:14 +0100

    [ADD] 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,14 +6,27 @@ import requests
 import sys
 
 
-if len(sys.argv) == 3:
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
+def list_commits(repository_name, owner_name):
+    url = f"""
+        https://api.github.com/repos/
+        {owner_name}/{repository_name}/commits?per_page=10"""
 
-    url = f"https://api.github.com/repos/{owner_name}/{repository_name}"
     response = requests.get(url)
 
     if response.status_code == 200:
-        print(response.json())
+        commits = response.json()
+        for commit in commits:
+            sha = commit["sha"]
+            author_name = commit["commit"]["author"]["name"]
+            print(f"{sha}: {author_name}")
     else:
-        print("Error: {}".format(response.status_code))
+        message = f"""
+            Error: Unable to fetch commits
+             (HTTP Status Code {response.status_code})"""
+
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    list_commits(repository_name, owner_name)
