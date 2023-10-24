767853c6b7f4a6ff90863a38d22bc20e04a1d6f4
Olamide Alokan <alokanolamide@gmail.com>
2023-10-24 14:25:44 +0100

    [ADD] 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
   if (error) {
