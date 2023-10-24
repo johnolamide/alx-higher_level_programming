ce275c27c42fd6ad1c97cd150e676a79eb66b962
Olamide Alokan <alokanolamide@gmail.com>
2023-10-24 15:01:33 +0100

    [ADD] 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,17 +5,16 @@ const url = process.argv[2];
 const characterId = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-        return;
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach(film => {
+    if (film.characters.includes(characterId)) {
+      count++;
     }
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes(characterId)) {
-        count++
-      }
-    });
-    console.log(count);
+  });
+  console.log(count);
 });
-
