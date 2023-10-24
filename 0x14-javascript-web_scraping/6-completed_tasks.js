3482f7aa1b999ab56dca7213f7515014d95372eb
Olamide Alokan <alokanolamide@gmail.com>
2023-10-24 15:35:12 +0100

    [ADD] 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
@@ -9,7 +8,7 @@ request(url, (error, response, body) => {
     console.log('error:', error);
     return;
   }
-  todos = JSON.parse(body);
+  const todos = JSON.parse(body);
   const completedTasks = {};
   todos.forEach(todo => {
     if (todo.completed) {
